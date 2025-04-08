f4e0e3ae3b12321070f114e0e6413e0b2b82850a
Jefferson Steven Echeverry Ramirez <jeffersonecheverry@Jeffersons-MacBook-Pro.local>
2025-04-07 19:32:51 -0500

    Added serviceCarousel

--- src/hooks/useCarouselImages.ts
+++ src/hooks/useCarouselImages.ts
@@ -1,13 +1,9 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import apiClient from "../api/axios";
 import { GetCarouselImages } from "../types/GetCarouselImages";
-import { API_ENDPOINTS } from "../api/endpoints";
 import { QUERY_KEYS } from "../api/queryKeys";
+import { serviceCarousel } from "../services/serviceCarousel";
 
-const fetchCarouselImages = async (): Promise<GetCarouselImages> => {
-    const response = await apiClient.get(API_ENDPOINTS.carousel.getCarouselImages);
-    return response.data;
-}
+const fetchCarouselImages = serviceCarousel.fetchCarouselImages;
 
 export const useCarouselImages = (): UseQueryResult<GetCarouselImages, Error> => {
     return useQuery<GetCarouselImages, Error, GetCarouselImages, [string]>({
