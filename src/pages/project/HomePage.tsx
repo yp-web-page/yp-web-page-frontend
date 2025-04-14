54cfa00a4747a3630be9c122b3ac5b3064417af5
Alexander940 <alexanderecheverry@outlook.es>
2025-04-13 20:32:34 -0500

    service information component implemented

--- src/pages/project/HomePage.tsx
+++ src/pages/project/HomePage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Carousel from '../../components/Carousel';
+import ServiceInformation from '../../components/ServiceInformation.tsx'
 
 const HomePage: React.FC = () => {
   return (
     <div className='font-bahamas-bold'>
       <Carousel />
-      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-title-light">Welcome to Yanca Publicity</h1>
-        <p className="mt-4 text-lg text-text-light">Your one-stop solution for all your publicity needs.</p>
-      </main>
+      <ServiceInformation />
     </div>
   );
 }
