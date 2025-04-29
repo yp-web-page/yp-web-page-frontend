f796a0181930fe83de6da19ded37e504ef7750e2
Alexander940 <alexanderecheverry@outlook.es>
2025-04-28 19:19:51 -0500

    component initiated

---
+++ src/components/products/ProductsCarousel.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const ProductsCarousel: React.FC = () => {
+    return(
+        <div>
+            <Swiper>
+                
+            </Swiper>
+        </div>
+    )
+}
+
+export default ProductsCarousel
