ebc05898c1c58935db52aac8bf28bdaebf188488
Alexander Echeverry Ramirez <alexanderecheverry@outlook.es>
2025-05-01 15:05:30 -0500

    Modal Provider import removed from Root Layout

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -2,7 +2,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { ModalProvider } from '../context/ModalContext';
 import ModalManager from '../components/modals/ModalManager';
 
 /**
