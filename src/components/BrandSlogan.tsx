d8eb3ffd6d51490ccd3ba7c0bba2c084f0c8c184
Jefferson Steven Echeverry Ramirez <jeffersonecheverry@192.168.1.7>
2025-05-24 12:05:28 -0500

    Chore: Added React.FC in slogan component

--- src/components/BrandSlogan.tsx
+++ src/components/BrandSlogan.tsx
@@ -1,4 +1,4 @@
-const BrandSlogan = () => (
+const BrandSlogan: React.FC = () => (
   <div className="w-full flex justify-center mt-12 mb-8">
     <span className="text-2xl md:text-3xl font-bahamas-bold text-gray-300 text-center tracking-wide uppercase">
       TU IMAGÍNALO, NOSOTROS LO HACEMOS REALIDAD
